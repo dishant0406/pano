8cc02e154c07dcdb4d9306113bc536c28c95a4dd
Dishant <dishu5570@gmail.com>
2023-12-22 19:19:31 +0530

    roadmap

--- Components/Micro/Step/index.tsx
+++ Components/Micro/Step/index.tsx
@@ -4,16 +4,8 @@ import React, { useState } from 'react'
 
 type Props = {}
 
-const Step = ({ title, left, completed, upcoming, noSubText }: {
-  title: string | JSX.Element,
-  left?: boolean,
-  completed?: boolean,
-  upcoming?: boolean,
-  noSubText?: boolean
-}) => {
-  const [isHover, setIsHover] = useState(false)
-  return (
-    <div onMouseEnter={() => {
+/*
+<div onMouseEnter={() => {
       setIsHover(true)
     }} onMouseLeave={() => {
       setIsHover(false)
@@ -28,6 +20,52 @@ const Step = ({ title, left, completed, upcoming, noSubText }: {
             upcoming ? '( Pending )' : completed ? '( Completed )' : '( On Going )'
           }</p>}
       </div>
+    </div>
+*/
+
+const Step = ({ title, left, completed, upcoming, noSubText }: {
+  title: string | JSX.Element,
+  left?: boolean,
+  completed?: boolean,
+  upcoming?: boolean,
+  noSubText?: boolean
+}) => {
+  const [isHover, setIsHover] = useState(false)
+  return (
+    <div className='md:w-[40vw] w-[90vw] flex relative items-center justify-center'>
+      <div className='h-[2rem] cursor-pointer relative w-[2rem] transition-all duration-300 hover:shadow-[0px_0px_45px_2px_rgba(244,_197,_92,_0.50)] rounded-full   bg-[#fcc054] flex items-center justify-center'></div>
+      {
+        !left && <div className='absolute w-full hidden md:flex flex-col items-end'>
+          <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[3vh] transition-all duration-300 `}>{title}</p>
+          {!noSubText && <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[2vh] transition-all duration-300 `}>{
+              upcoming ? '( Pending )' : completed ? '( Completed )' : '( On Going )'
+            }</p>}
+        </div>
+      }
+
+
+      {
+        left && <div className='absolute w-full hidden md:flex flex-col items-start'>
+          <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[3vh] transition-all duration-300 `}>{title}</p>
+          {!noSubText && <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[2vh] transition-all duration-300 `}>{
+              upcoming ? '( Pending )' : completed ? '( Completed )' : '( On Going )'
+            }</p>}
+        </div>
+      }
+      {
+        <div className='absolute w-full md:hidden bg-black top-[-9vh] flex flex-col items-center'>
+          <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[3vh] transition-all duration-300 `}>{title}</p>
+          {!noSubText && <p className={`text-white ${upcoming ? 'opacity-50' : (!completed && !isHover) ? 'opacity-80' : 'opacity-100'
+            } whitespace-nowrap text-[2vh] transition-all duration-300 `}>{
+              upcoming ? '( Pending )' : completed ? '( Completed )' : '( On Going )'
+            }</p>}
+        </div>
+      }
     </div>
   )
 }
